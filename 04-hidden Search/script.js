879dcfdaa0234a1c3e53da6a6bf6776e3e8e6e89
sayedhany <sayedh@mail.ru>
2023-03-18 13:18:22 +0200

    add some feature

--- 04-hidden Search/script.js
+++ 04-hidden Search/script.js
@@ -1,6 +1,21 @@
 const search = document.querySelector(".search");
 const btn = document.querySelector(".btn");
+const form = document.querySelector("form");
+const body = document.querySelector("body");
 
 btn.addEventListener("click", () => {
   search.classList.toggle("active");
+});
+
+form.addEventListener("click", (e) => {
+  e.preventDefault();
+});
+
+body.addEventListener("click", (e) => {
+  console.log(e.target);
+  if (e.target.classList[0]) {
+    return;
+  } else {
+    search.classList.remove("active");
+  }
 });
