a2fe0fb3c0b09d3c404936b90d0ff36662476a22
sayedhany <sayedh@mail.ru>
2023-04-02 22:50:03 +0200

    README file

--- 24-Content Placeholder/script.js
+++ 24-Content Placeholder/script.js
@@ -1,3 +1,12 @@
+/*
+ * This code selects some HTML elements by their IDs and assigns them to variables. The selected elements include the header, title, excerpt, profile image, name, and date. Additionally, this code will select all elements with class "animated-bg" and "animated-bg-text" using querySelectorAll method.
+
+Next, there is a function called getData() that sets the innerHTML of each of these selected elements to certain values. Specifically, header gets a new image, title gets some text, excerpt gets some more text, profile_img gets an image of John Doe, name gets the name "John Doe", and date gets the string "Oct 08, 2020".
+
+Finally, in this function, we remove the classes "animated-bg" and "animated-bg-text" from all elements with these classes using the forEach method.
+
+Overall, this code appears to be setting up a webpage with a specific design for the data, which is activated after 2.5 seconds through the setTimeout function.
+ */
 const header = document.getElementById("header");
 const title = document.getElementById("title");
 const excerpt = document.getElementById("excerpt");
