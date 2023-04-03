e6653d6da104c2f601a8376fa24309927a5ea85a
sayedhany <sayedh@mail.ru>
2023-04-03 16:11:37 +0200

    added

---
+++ 25-Sticky Navigation/script.js
@@ -0,0 +1,33 @@
+/**
+ * The code defines a fixed navigation bar that will become active as the user scrolls down the page.
+
+The first line selects the navigation element with class .nav from the HTML document and assigns it to the variable nav. The second line selects all links inside list items (<li>) within the ul element, and assigns them to the links variable.
+
+Next, an event listener is added to the window using addEventListener(), which listens for the 'scroll' event and triggers the fixNav() function whenever the event occurs.
+
+The fixNav() function logs the current scroll position of the window and the height of the navigation bar to the console. Then, it checks if the current scroll position is greater than the sum of the navigation bar height plus 150 pixels. If it is, it adds the class active to the navigation element so that it becomes fixed at the top of the page. If it's not, it removes the class active, so the navigation element is returned to its original position.
+
+Lastly, two forEach() loops iterate through all the links elements, removing the current class from each one, then adding a click event to each link that, when clicked, removes the current class from all other links in links, and adds the current class to the clicked link. This updates the visual appearance of the currently selected link in the navigation menu.
+ */
+const nav = document.querySelector(".nav");
+const links = document.querySelectorAll("ul li a");
+window.addEventListener("scroll", fixNav);
+
+function fixNav() {
+  console.log(window.scrollY, nav.offsetHeight);
+  if (window.scrollY > nav.offsetHeight + 150) {
+    nav.classList.add("active");
+  } else {
+    nav.classList.remove("active");
+  }
+}
+
+links.forEach((link) => {
+  link.classList.remove("current");
+});
+links.forEach((link) => {
+  link.addEventListener("click", (e) => {
+    links.forEach((link1) => link1.classList.remove("current"));
+    link.classList.add("current");
+  });
+});
