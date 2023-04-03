91f7dae7567c73a05c0b08b97f3a4fd1ee8e1cb3
sayedhany <sayedh@mail.ru>
2023-04-03 17:06:36 +0200

    solved

--- 26-Double Vertical Slider/script.js
+++ 26-Double Vertical Slider/script.js
@@ -32,17 +32,19 @@ function changSlide(direction) {
     activeSlideIndex++;
     if (activeSlideIndex > slidesLength - 1) {
       activeSlideIndex = 0;
-    } else if (direction === "down") {
-      activeSlideIndex--;
-      if (activeSlideIndex < 0) {
-        activeSlideIndex = slidesLength - 1;
-      }
     }
-    slideRight.style.transform = `translateY(-${
-      activeSlideIndex * sliderHeight
-    }px)`;
-    slideLeft.style.transform = `translateY(${
-      activeSlideIndex * sliderHeight
-    }px)`;
+  } else if (direction === "down") {
+    console.log("here");
+    activeSlideIndex--;
+    if (activeSlideIndex < 0) {
+      activeSlideIndex = slidesLength - 1;
+    }
   }
+
+  slideRight.style.transform = `translateY(-${
+    activeSlideIndex * sliderHeight
+  }px)`;
+  slideLeft.style.transform = `translateY(${
+    activeSlideIndex * sliderHeight
+  }px)`;
 }
